af0cbd3b11f4fc8cb491ddf20f8b0e7e390d91b0
amoani06 <amoanimaa07@gmail.com>
2026-05-29 20:19:03 +0000

    update

---
+++ wp-content/plugins/sina-extension-for-elementor/inc/templates/single.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Single Template.
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+get_header();
+?>
+<main id="content" <?php post_class( 'site-main' ); ?>>
+	<?php do_action( 'sina_ext_single_builder_content' ); ?>
+</main>
+<?php
+get_footer();
+
